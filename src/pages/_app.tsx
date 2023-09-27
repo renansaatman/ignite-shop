0449c102d7314b3b60e87affa80dcef927991bfd
Renan Saatman <renansaatman@hotmail.com>
2023-09-27 18:34:44 -0300

    update: cart dialog added and stylized

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,74 @@ import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 
 import logoImg from '../assets/logo.svg'
-import { Button, CartItems, Container, Header } from "../styles/pages/app"
+import shirt from '../assets/shirt.jpg'
+import { Button, CardInfo, CartItem, CartClose, CartContent, ItemsQuantity, Container, Header, CartItems, TotalItems, TotalPrice, FinishButton, PriceInfo } from "../styles/pages/app"
 import Image from "next/image"
 
-import { Handbag } from '@phosphor-icons/react'
+import { Handbag, X } from '@phosphor-icons/react'
+
+import * as Dialog from '@radix-ui/react-dialog'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  
   return (
     <Container>
       <Header>
         <Image src={logoImg} alt="" />
-        <Button cart={true ? 'load' : 'empty'}>
-          <Handbag size={24} weight="bold" />
-          <CartItems>1</CartItems>
-        </Button>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <Button cart={false ? 'load' : 'empty'}>
+              <Handbag size={24} weight="bold" />
+              {/* <ItemsQuantity>1</ItemsQuantity> */}
+            </Button>
+          </Dialog.Trigger>
+          <CartContent>
+            <CartClose>
+              <X size={24} weight="bold"/>
+            </CartClose>
+
+            <h2>Sacola de compras</h2>
+            <CartItems>
+              <CartItem>
+                <Image src={shirt} width={101.94} height={93} alt="" />
+                <CardInfo>
+                  <h3>Camiseta Beyond the Limits</h3>
+                  <span>R$ 79,90</span>
+                  <button>Remover</button>
+                </CardInfo>
+              </CartItem>
+              <CartItem>
+                <Image src={shirt} width={101.94} height={93} alt="" />
+                <CardInfo>
+                  <h3>Camiseta Beyond the Limits</h3>
+                  <span>R$ 79,90</span>
+                  <button>Remover</button>
+                </CardInfo>
+              </CartItem>
+              <CartItem>
+                <Image src={shirt} width={101.94} height={93} alt="" />
+                <CardInfo>
+                  <h3>Camiseta Beyond the Limits</h3>
+                  <span>R$ 79,90</span>
+                  <button>Remover</button>
+                </CardInfo>
+              </CartItem>
+            </CartItems>
+
+            <PriceInfo>
+              <TotalItems>
+                <span>Quantidade</span>
+                <span className="items">3 itens</span>
+              </TotalItems>
+              <TotalPrice>
+                <span>Valor total</span>
+                <span className="price">R$ 270,00</span>
+              </TotalPrice>
+              <FinishButton>Finalizar compra</FinishButton>
+            </PriceInfo>
+          </CartContent>
+        </Dialog.Root>
       </Header>
       <Component {...pageProps} />
     </Container>
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -1,4 +1,17 @@
-import { styled } from "..";
+import { styled } from ".."
+import * as Dialog from '@radix-ui/react-dialog'
+
+import { keyframes } from '@stitches/react'
+
+const slideInFromRight = keyframes({
+  from: { transform: 'translateX(100%)' },
+  to: { transform: 'translateX(0)' }
+})
+
+const slideOutToRight = keyframes({
+  from: { transform: 'translateX(0)' },
+  to: { transform: 'translateX(100%)' },
+})
 
 export const Container = styled('div', {
   display: "flex",
@@ -46,7 +59,7 @@ export const Button = styled('button', {
   }
 })
 
-export const CartItems = styled('span', {
+export const ItemsQuantity = styled('span', {
   width: 27,
   height: 27,
   borderRadius: 999,
@@ -63,4 +76,168 @@ export const CartItems = styled('span', {
   position: 'absolute',
   top: -8,
   right: -8.345,
+})
+
+export const CartContent = styled(Dialog.Content, {
+  display: 'flex',
+  flexDirection: 'column',
+
+  position: 'fixed',
+  top: 0,
+  right: 0,
+  bottom: 0,
+
+  width: '30rem',
+  backgroundColor: '$gray800',
+  boxShadow: '-4px 0px 30px rgba(0, 0, 0, 0.8)',
+  padding: '4.5rem 3rem 3rem 3rem',
+
+  zIndex: 99,
+
+  animation: `${slideInFromRight} 0.3s ease-in-out`,
+  '&[data-state="closed"]': {
+    animation: `${slideOutToRight} 0.3s ease-in-out`,
+  },
+
+  h2: {
+    fontSize: '$lg',
+    color: '$gray100',
+
+    marginBottom: '2rem',
+  },
+
+  img: {
+    borderRadius: 8,
+  }
+})
+
+export const CartItems = styled('div', {
+  height: 330,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.5rem',
+  overflowY: 'auto',
+
+  marginBottom: '12.375rem'
+})
+
+export const CartItem = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '1.25rem'
+})
+
+export const CardInfo = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+
+  h3: {
+    color: '$gray100',
+    fontSize: '$md',
+    fontWeight: 'normal',
+    lineHeight: 1.6,
+  },
+
+  span: {
+    color: '$gray100',
+    fontWeight: 'bold',
+    fontSize: '$md',
+    lineHeight: 1.6,
+
+    marginBottom: '.5rem'
+  },
+
+  button: {
+    border: 0,
+    background: 'none',
+    color: '$green500',
+    fontWeight: 'bold',
+    
+    transition: 'color .2s',
+    
+    '&:hover': {
+      color: '$green300'
+    },
+    cursor: 'pointer',
+  }
+})
+
+export const CartClose = styled(Dialog.Close, {
+  position: 'absolute',
+  top: '1.75rem',
+  right: '1.75rem',
+
+  border: 0,
+  background: 'none',
+  color: '$gray500',
+
+  cursor: 'pointer'
+})
+
+export const TotalItems = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  marginBottom: 7,
+
+  span: {
+    color: '$gray100',
+    lineHeight: 1.6,
+  },
+
+  '.items': {
+    color: '$gray300',
+    fontSize: '$md',
+  } 
+})
+
+export const TotalPrice = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  span: {
+    color: '$gray100',
+    lineHeight: 1.6,
+    fontWeight: 'bold',
+    fontSize: '$md'
+  },
+
+  '.price': {
+    fontSize: '$xl',
+  },
+  marginBottom: 57,
+})
+
+export const FinishButton = styled('button', {
+  border: 0,
+  borderRadius: 8,
+  backgroundColor: '$green500',
+  transition: 'background-color .2s',
+  padding: '1.25rem',
+  cursor: 'pointer',
+  fontWeight: 'bold',
+  fontSize: '$md',
+  color: '$white',
+
+  '&:not(:disabled):hover': {
+    backgroundColor: '$green300',
+  },
+
+  '&:disabled': {
+    opacity: 0.6,
+    cursor: 'not-allowed',
+  }
+})
+
+
+export const PriceInfo = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  position: 'absolute',
+  bottom: '3rem',
+  right: '3rem',
+  left: '3rem',
 })
